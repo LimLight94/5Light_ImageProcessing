6fa32b2f63e046fb6a07b46146faf0b82e5754fa
Choongseop <kchs94@gmail.com>
2021-01-28 14:20:40 +0900

    Add remove margin

--- ImageProcessing_py/panorama/videopanorama.py
+++ ImageProcessing_py/panorama/videopanorama.py
@@ -1,11 +1,13 @@
-
+import matplotlib.pyplot as plt
 import argparse
-import logging
-import cv2
+import logging #디버그할 때 로그찍어주는 모듈
+import cv2  #opencv 모듈
+import numpy as np
+import sys
 
 from ImageProcessing_py import utils
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # 플러그
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('video_path', type=str, help="paths to one or more images or image directories")
     parser.add_argument('-b', '--debug', dest='debug', action='store_true', help='enable debug logging')
@@ -18,7 +20,7 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--min', dest='min_correspondence', default=10, type=int, help='min correspondences')
     args = parser.parse_args()
 
-    if args.debug:
+    if args.debug: # 예외처리
         logging.basicConfig(level=logging.DEBUG)
     else:
         logging.basicConfig(level=logging.INFO)
@@ -26,7 +28,7 @@ if __name__ == '__main__':
 
     logging.info("beginning sequential matching")
 
-    if utils.helpers.is_cv2():
+    if utils.helpers.is_cv2():     #opencv 버전 별
         sift = cv2.SIFT()
     elif utils.helpers.is_cv3():
         sift = cv2.xfeatures2d.SIFT_create()
@@ -50,9 +52,10 @@ if __name__ == '__main__':
             continue
         i = i + 1
 
-        if not ret:
+
+        if not ret: #제대로 못읽었을 경우
             break
-        else:
+        else:       #제대로 읽었을 경우 그레이로 바꾸기
             frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         if result is None:
@@ -83,3 +86,35 @@ if __name__ == '__main__':
     cv2.destroyAllWindows()
     if args.save:
         utils.helpers.save_image(args.video_path, result)
+
+
+# 이미지가 불러져왔는지 확인
+image = cv2.imread("C:/All/repos/5Light_ImageProcessing/res/panorama/test.png", cv2.IMREAD_ANYCOLOR)
+if image is None:
+    print('Imager load failed!') # 이미지가 없으면 출력
+    sys.exit()
+cv2.imshow("panorama image", image)
+
+# 마스크 구하기(픽셀값 0,255로 나누기)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+thresh = cv2.bitwise_not(cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)[1])
+thresh = cv2.medianBlur(thresh, 5)
+
+plt.figure(figsize=(20, 20))
+plt.imshow(thresh, cmap='gray')
+
+
+# 테두리 지우기
+stitched_copy = image.copy()
+thresh_copy = thresh.copy()
+
+while np.sum(thresh_copy) > 0:
+    thresh_copy = thresh_copy[1:-1, 1:-1]
+    stitched_copy = stitched_copy[1:-1, 1:-1]
+
+plt.figure(figsize=(20, 20))
+plt.imshow(cv2.cvtColor(stitched_copy, cv2.COLOR_BGR2RGB))
+cv2.imshow("clean image", stitched_copy)
+cv2.imwrite("C:/All/repos/5Light_ImageProcessing/res/panorama/result.png",stitched_copy)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
