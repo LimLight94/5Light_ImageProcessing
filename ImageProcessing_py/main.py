b23cd0a03315ba409bdd718b6fcd87b8b910b442
jj441 <jspooon@gmail.com>
2021-05-25 17:15:06 +0900

    test code append

--- ImageProcessing_py/main.py
+++ ImageProcessing_py/main.py
@@ -43,7 +43,7 @@ if __name__ == '__main__':  # 플러그
     flann = cv2.FlannBasedMatcher({'algorithm': 1, 'trees': 5}, {'checks': 50})
 
     cap = cv2.VideoCapture(args.video_path)
-    beforeResult = cv2.imread("res/0511sam/result6.jpg")
+    beforeResult = cv2.imread("res/sam/0520sam/result10-1.jpg")
     beforeResult_gray = cv2.cvtColor(beforeResult, cv2.COLOR_BGR2GRAY)
     length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -56,37 +56,66 @@ if __name__ == '__main__':  # 플러그
     i = 0
     resultT = None
     features1 = None
-    #meanTime = 0
     h, w, f = 404, 720, 950
     K = np.array([[f, 0, w / 2], [0, f, h / 2], [0, 0, 1]], dtype=np.float32)
     angles = math.pi / 180
     theta = [angles * 0, angles * 45, angles * 45]
     R = utils.helpers.eulerAnglesToRotationMatrix(theta)
     #print("R : " + str(R))
-    #video = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc(*'DIVX'), 30, (beforeResult.shape[1],beforeResult.shape[0]))
+    #video = cv2.VideoWriter('output.mp4', cv2.VideoWriter_fourcc(*'MP4V'), 10, (beforeResult.shape[1],beforeResult.shape[0]))
     tm = cv2.TickMeter()
 
     object_detector = Detection()
     i = 0
+    meanTime1 = 0
+    meanTime2 = 0
+    meanTime3 = 0
+    meanTime4 = 0
+    j = 0
     while True:
-        #tm.reset()
-        #tm.start()
-        #t1 = time.time()
+
+
         ret, frame = cap.read()
+        if i % 3 != 0:
+            i = i + 1
+            continue
         i = i + 1
-        print(str(i))
+
         if not ret:  # 제대로 못읽었을 경우
             break
         else:  # 제대로 읽었을 경우 그레이로 바꾸기
+            j += 1
+
             if frame.shape[1] > 720:
                 frame = imutils.resize(frame, width=720)
 
+            tm.reset()
+            tm.start()
+            t1 = time.time()
+
             warper = cv2.PyRotationWarper('spherical', float(f))
             corner, frame = warper.warp(frame, K, R, cv2.INTER_LINEAR, cv2.BORDER_CONSTANT)
+
+            tm.stop()
+            ms = tm.getTimeSec()  # 밀리 초 단위 시간을 받아옴
+            meanTime1 += ms
+            print('spherical warp mean time : {}s.'.format(meanTime1 / j))
+
             frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+            tm.reset()
+            tm.start()
+            t1 = time.time()
             #detection
             frame = object_detector.detectObject(frame)
+            tm.stop()
+            ms = tm.getTimeSec()  # 밀리 초 단위 시간을 받아옴
+            meanTime2 += ms
+            print('detection mean time : {}s.'.format(meanTime2 / j))
+
+            tm.reset()
+            tm.start()
+            t1 = time.time()
 
             features0 = sift.detectAndCompute(frame_gray, None)
             if features1 is None:
@@ -95,20 +124,29 @@ if __name__ == '__main__':  # 플러그
             matches_src, matches_dst, n_matches = utils.compute_matches(
                 features0, features1, flann, knn=args.knn)
 
+            tm.stop()
+            ms = tm.getTimeSec()  # 밀리 초 단위 시간을 받아옴
+            meanTime3 += ms
+            print('feature mean time : {}s.'.format(meanTime3 / j))
+
             if n_matches < args.min_correspondence:
                 logger.error("error! too few correspondences")
                 continue
 
+            tm.reset()
+            tm.start()
+            t1 = time.time()
             H, mask = cv2.findHomography(matches_src, matches_dst, cv2.RANSAC, 5.0)
 
             if resultT is None:
                 h0, w0 = beforeResult.shape[0:2]
                 resultT = np.zeros((h0, w0, 3), np.uint8)
             resultT, contours = utils.combine_images(beforeResult, frame, H, resultT)
-            #tm.stop()
-            #ms = tm.getTimeSec()  # 밀리 초 단위 시간을 받아옴
+            tm.stop()
+            ms = tm.getTimeSec()  # 밀리 초 단위 시간을 받아옴
+            meanTime4 += ms
+            print('mean time : {}s.'.format(meanTime4 / j))
 
-            #print('time: {}s.'.format(tm.getTimeSec()))
             img = utils.display_red("result", resultT, contours)
             cv2.waitKey(1)
             #video.write(img)
